19e6eec18bf726e0f17e5c70e9a515443d1be4a5
Sthefany Albuquerque <s.albuquerque@aluno.ifsp.edu.br>
2025-03-29 06:01:14 -0300

    Nenhuma alteração realizada no código.

--- bibliotecas_customizadas/Libraries/ExcelLibrary/excel_library.py
+++ bibliotecas_customizadas/Libraries/ExcelLibrary/excel_library.py
@@ -1,6 +1,35 @@
 import openpyxl
 from robot.api.deco import keyword
 
+"""
+Biblioteca personalizada para manipulação de planilhas Excel usando openpyxl.
+
+Esta biblioteca fornece keywords para interagir com planilhas Excel no formato .xlsx.
+Ela permite carregar planilhas, buscar dados por tamanho, raça e temperamento, além de contar animais por temperamento.
+
+Dependência:
+    openpyxl: Biblioteca do Python para ler e escrever arquivos Excel (.xlsx).
+
+Funções:
+    - Carregar Planilha: Carrega a planilha e retorna o objeto WorkBook.
+    - Buscar Animais Por Tamanho: Retorna uma lista de animais que possuem o tamanho especificado.
+    - Buscar Animal Por Raça: Retorna os detalhes de uma raça específica.
+    - Contar Animais Por Temperamento: Conta quantos animais possuem o temperamento especificado.
+
+Exemplo de uso:
+    Exemplo de uso:
+    arquivo = "./bibliotecas_customizadas/tests/my_custom_libraries_excel/exem_planilha_testes_automatizados_2.xlsx"  # Substitua pelo caminho real do seu arquivo
+    planilha_dados = carregar_planilha(arquivo)
+
+    Chamando as funções
+    print("\nBuscar Animais Por Tamanho:")
+    print(buscar_por_tamanho(planilha_dados, "pequeno"))
+    print("\nBuscar Animais Por Raça:")
+    print(buscar_por_raca(planilha_dados, "Pinscher"))
+    print("\nContar Animais Por Temperamento:")
+    print(contar_por_temperamento(planilha_dados, "Atento"), "\n")
+"""
+
 @keyword("Carregar Planilha")
 def carregar_planilha(arquivo):
     """Carrega a planilha e retorna o objeto WorkBook."""
@@ -34,15 +63,3 @@ def contar_por_temperamento(WorkBook, temperamento):
         if row[3].lower() == temperamento.lower():
             contador += 1
     return contador
-
-# Exemplo de uso:
-# arquivo = "./bibliotecas_customizadas/tests/my_custom_libraries_excel/exem_planilha_testes_automatizados_2.xlsx"  # Substitua pelo caminho real do seu arquivo
-# planilha_dados = carregar_planilha(arquivo)
-
-# Chamando as funções
-# print("\nBuscar Animais Por Tamanho:")
-# print(buscar_por_tamanho(planilha_dados, "pequeno"))
-# print("\nBuscar Animais Por Raça:")
-# print(buscar_por_raca(planilha_dados, "Pinscher"))
-# print("\nContar Animais Por Temperamento:")
-# print(contar_por_temperamento(planilha_dados, "Atento"), "\n")
